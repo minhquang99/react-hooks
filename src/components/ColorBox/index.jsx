31f86d18cedd909954030e22c4139be4a49dc962
ASUS <ASUS@LAPTOP-D8LBK4KJ>
2021-07-27 02:02:19 +0700

    up

---
+++ src/components/ColorBox/index.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './ColorBox.scss';
+
+ColorBox.propTypes = {
+
+};
+
+function getRandomColor() {
+    const LIST_COLOR = ['deeppink', 'yellow', 'blue', 'black', 'purple'];
+    const randomColor = Math.trunc(Math.random() * 5);
+    return LIST_COLOR[randomColor];
+}
+
+function ColorBox() {
+
+    const initColor = localStorage.getItem('box_color') || 'deeppink';
+    console.log(initColor);
+    const [color, setColor] = useState(() => {
+        const initColor = localStorage.getItem('box_color') || 'deeppink';
+        console.log(initColor);
+        return initColor;
+    });
+
+    const onBoxClick = () => {
+        const newColor = getRandomColor();
+        setColor(newColor);
+
+        localStorage.setItem('box_color', newColor);
+    }
+
+    return (
+        <div
+            className="color-box"
+            style={{ backgroundColor: color }}
+            onClick={onBoxClick}
+        >
+
+        </div>
+    );
+}
+
+export default ColorBox;
