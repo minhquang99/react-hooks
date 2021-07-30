68eb7cc371b5e6ba6f584cc302f408aa9e010cb0
ASUS <ASUS@LAPTOP-D8LBK4KJ>
2021-07-30 20:45:59 +0700

    custom hooks

--- src/components/Clock/index.jsx
+++ src/components/Clock/index.jsx
@@ -1,36 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import useClock from '../../hooks/useClock';
 
 Clock.propTypes = {
 
 };
 
-function formatDate(date) {
-
-    const hours = `0${date.getHours()}`.slice(-2);
-    const minutes = `0${date.getMinutes()}`.slice(-2);
-    const seconds = `0${date.getSeconds()}`.slice(-2);
-
-    return `${hours}:${minutes}:${seconds}`;
-}
-
 function Clock(props) {
 
-    const [timeString, setTimeString] = useState('');
-
-    useEffect(() => {
-        const clockInterval = setInterval(() => {
-            const now = new Date();
-            const newTimeString = formatDate(now);
-            setTimeString(newTimeString);
-        }, 1000);
-
-        //clean up de khi unmount ko setTimeString...
-        return () => {
-            console.log('Clean up');
-            clearInterval(clockInterval);
-        }
-    }, []);
+    const { timeString } = useClock();
     return (
         <h3>{`Time now: ${timeString}`}</h3>
     );
